0745c0fd2a3aac4fb6a2402ba8842a6f5a8caa24
Alingof <Alignof@outlook.com>
2023-08-30 17:45:41 +0900

    [add] add a functions start and panic_handler

--- src/start.rs
+++ src/start.rs
@@ -1,1 +1,15 @@
-pub fn start() {}
+use riscv::register::{sstatus, stvec};
+
+/// Start function
+pub fn start(hart_id: u64, dtb_addr: u64) {
+    unsafe {
+        sstatus::clear_sie();
+        stvec::write(
+            panic_handler as *const fn() as usize,
+            stvec::TrapMode::Direct,
+        );
+    }
+}
+
+/// Panic handler
+fn panic_handler() {}
