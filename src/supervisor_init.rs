fd351cdd9fb55d4ead61801971d0dc9d589228c6
Alingof <Alignof@outlook.com>
2023-10-04 18:00:07 +0900

    [add] prepare the boot page tables

--- src/supervisor_init.rs
+++ src/supervisor_init.rs
@@ -1,6 +1,6 @@
 use crate::memmap::constant::{
-    DRAM_BASE, DRAM_SIZE_PAR_HART, PA2VA_DRAM_OFFSET, PAGE_SIZE, PAGE_TABLE_BASE,
-    PAGE_TABLE_OFFSET_PER_HART, STACK_BASE, STACK_SIZE_PER_HART,
+    DRAM_BASE, DRAM_SIZE_PAR_HART, PA2VA_DEVICE_OFFSET, PA2VA_DRAM_OFFSET, PAGE_SIZE,
+    PAGE_TABLE_BASE, PAGE_TABLE_OFFSET_PER_HART, STACK_BASE,
 };
 use crate::memmap::device::plic::{
     CONTEXT_BASE, CONTEXT_CLAIM, CONTEXT_PER_HART, ENABLE_BASE, ENABLE_PER_HART,
@@ -128,20 +128,37 @@ fn smode_setup(hart_id: usize, dtb_addr: usize) {
             .write_volatile(0);
     }
 
+    // copy fdt to 0x9000_0000
+
     unsafe {
         // set sie = 0x222
         sie::set_ssoft();
         sie::set_stimer();
         sie::set_sext();
 
-        // satp = Sv39 | 0x8000_0000 >> 12
-        satp::set(
-            satp::Mode::Sv39,
-            0,
-            (DRAM_BASE + DRAM_SIZE_PAR_HART * hart_id) >> 12,
-        );
+        // boot page tables
+        let guest_id = hart_id + 1;
+        let guest_base_addr = DRAM_BASE + guest_id * DRAM_SIZE_PAR_HART;
+        let page_table_start = guest_base_addr;
+        for pt_index in 0..1024 {
+            let pt_offset = (page_table_start + pt_index * 8) as *mut usize;
+            pt_offset.write_volatile(match pt_index {
+                // 0xffff_fffc_0xxx_xxxx ..= 0xffff_ffff_8xxx_xxxx
+                496..=503 => (pt_index - 496) << 28 | 0xcf, // a, d, x, w, r, v
+                // 0x0000_0000_8xxx_xxxx or 0xffff_ffff_cxxx_xxxx
+                2 | 511 => (PAGE_TABLE_BASE + PAGE_SIZE) >> 2 | 0x01, // v
+                // 2 and 511 point to 512 PTE
+                512 => 0x2000_0000 | 0xcb, // d, a, x, r, v
+                // 2nd level
+                513..=1023 => (0x2000_0000 + ((pt_index - 512) << 19)) | 0xc7, // d, a, w, r, v
+                _ => 0,
+            });
+        }
 
-        let stack_pointer = STACK_BASE + STACK_SIZE_PER_HART * hart_id + PA2VA_DRAM_OFFSET;
+        // satp = Sv39 | 0x9000_0000 >> 12
+        satp::set(satp::Mode::Sv39, 0, guest_base_addr >> 12);
+
+        let stack_pointer = guest_base_addr + 0x20_0000 + PA2VA_DEVICE_OFFSET;
         asm!("mv a0, {dtb_addr}", dtb_addr = in(reg) dtb_addr);
         asm!("mv sp, {stack_pointer_in_umode}", stack_pointer_in_umode = in(reg) stack_pointer);
         asm!("j {enter_user_mode}", enter_user_mode = sym enter_user_mode);
