8bd10ff21bce508919b2e15f56f8c76d9891dfb5
Alignof <alignof@outlook.com>
2023-10-14 02:44:34 +0900

    [fix] fix oprand value of loading a7

--- src/trap/machine.rs
+++ src/trap/machine.rs
@@ -49,7 +49,7 @@ pub unsafe fn mtrap_vector() {
     let a0: u64 = 0;
     let a7: u64 = 0;
     asm!("ld {a0_reg}, 64(sp)", a0_reg = in(reg) a0);
-    asm!("ld {a7_reg}, 64(sp)", a7_reg = in(reg) a7);
+    asm!("ld {a7_reg}, 120(sp)", a7_reg = in(reg) a7);
     match mcause::read().cause() {
         Trap::Interrupt(interrupt_cause) => trap_interrupt(interrupt_cause),
         Trap::Exception(exception_cause) => trap_exception(a0, a7, exception_cause),
