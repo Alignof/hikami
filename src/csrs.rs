0f87aee62fb4ae1d5ec5774297c1261137fd4968
Alingof <Alignof@outlook.com>
2024-05-25 16:32:02 +0900

    [add] add `read_csr_as` macro

--- src/csrs.rs
+++ src/csrs.rs
@@ -1,7 +1,28 @@
 //! Define CSRs that are not in the riscv crate.
 
+#[macro_export]
+macro_rules! read_csr_as {
+    ($register:ident, $csr_number:literal) => {
+        #[inline]
+        pub fn read() -> $register {
+            $register {
+                bits: {
+                    let csr_out;
+                    unsafe {
+                        core::arch::asm!(concat!("csrrs {0}, ", stringify!($csr_number), ", x0"), out(reg) csr_out);
+                    }
+                    csr_out
+                }
+            }
+        }
+    };
+}
+
 mod hvip {
+    const HVIP: usize = 0x645;
     struct Hvip {
         bits: usize,
     }
+
+    read_csr_as!(Hvip, 0x645);
 }
