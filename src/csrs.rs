fb9a5e4fca88149270cd7b7a9979c89f60c12443
Alignof <alignof@outlook.com>
2024-05-28 00:01:41 +0900

    [add] add hedeleg and hideleg module

--- src/csrs.rs
+++ src/csrs.rs
@@ -1,4 +1,6 @@
 //! Define CSRs that are not in the [riscv crate](https://crates.io/crates/riscv).
+//!
+//! The specification referred to "The RISC-V Instruction Set Manual: Volume II Version 20240411".
 
 /// Implement reading CSR method to the struct.
 #[macro_export]
@@ -41,6 +43,55 @@ pub mod vsatp {
 
     read_csr_as!(Vsatp, 0x280);
     write_csr_as!(0x280);
+}
+
+pub mod hedeleg {
+    //! Hypervisor exception delegation register.
+    const HEDELEG: usize = 0x602;
+    pub struct Hedeleg {
+        bits: usize,
+    }
+
+    /// Exception Kind that possible to delegate to lower privileged.
+    ///
+    /// Ref: The RISC-V Instruction Set Manual: Volume II Version 20240411, p132 Table 29.
+    pub enum ExceptionKind {
+        /// Instruction address misaligned (bit 0)
+        InstructionAddressMissaligned = 0x1,
+        /// Breakpoint (bit 3)
+        Breakpoint = 0x8,
+        /// Environment call from U-mode or VU-mode (bit 8)
+        EnvCallFromUorVU = 0x100,
+        /// Instruction page fault (bit 12)
+        InstructionPageFault = 0x1000,
+        /// Load page fault (bit13)
+        LoadPageFault = 0x2000,
+        /// Store AMO page fault (bit 15)
+        StoreAmoPageFault = 0x8000,
+    }
+
+    read_csr_as!(Hedeleg, 0x602);
+    write_csr_as!(0x602);
+}
+
+pub mod hideleg {
+    //! Hypervisor interrupt delegation register.
+    const HIDELEG: usize = 0x603;
+    pub struct Hideleg {
+        bits: usize,
+    }
+
+    pub enum InterruptKind {
+        /// VS-level external interrupts (bit 10)
+        Vsei,
+        /// VS-level timer interrupts (bit 6)
+        Vsti,
+        /// VS-level software interrupts (bit 2)
+        Vssi,
+    }
+
+    read_csr_as!(Hideleg, 0x603);
+    write_csr_as!(0x603);
 }
 
 pub mod hvip {
--- src/hypervisor_init.rs
+++ src/hypervisor_init.rs
@@ -1,4 +1,5 @@
-use crate::csrs::{hvip, vsatp};
+use crate::csrs::{hedeleg, hedeleg::ExceptionKind, hideleg, hideleg::InterruptKind, hvip, vsatp};
+use riscv::register::sie;
 
 #[inline(never)]
 pub extern "C" fn init_hypervisor(hart_id: usize, _dtb_addr: usize) {
@@ -10,4 +11,25 @@ pub extern "C" fn init_hypervisor(hart_id: usize, _dtb_addr: usize) {
 
     // disable address translation.
     vsatp::write(0);
+
+    // set sie = 0x222
+    unsafe {
+        sie::set_ssoft();
+        sie::set_stimer();
+        sie::set_sext();
+    }
+
+    // specify delegation exception kinds.
+    hedeleg::write(
+        ExceptionKind::InstructionAddressMissaligned as usize
+            | ExceptionKind::Breakpoint as usize
+            | ExceptionKind::EnvCallFromUorVU as usize
+            | ExceptionKind::InstructionPageFault as usize
+            | ExceptionKind::LoadPageFault as usize
+            | ExceptionKind::StoreAmoPageFault as usize,
+    );
+    // specify delegation interrupt kinds.
+    hideleg::write(
+        InterruptKind::Vsei as usize | InterruptKind::Vsti as usize | InterruptKind::Vssi as usize,
+    );
 }
