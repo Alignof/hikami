cde424f76f74159859599d38dc3574e41a0c13b8
Alingof <Alignof@outlook.com>
2024-06-21 14:21:32 +0900

    [add] implement `dram_base` to `Guest`

--- src/guest.rs
+++ src/guest.rs
@@ -1,4 +1,7 @@
 /// Handling guest OS.
+use crate::memmap::constant::{
+    DRAM_BASE, DRAM_SIZE_PAR_HART, GUEST_TEXT_OFFSET, PA2VA_DRAM_OFFSET,
+};
 
 /// Guest Information
 struct Guest {
@@ -9,5 +12,10 @@ struct Guest {
 impl Guest {
     pub fn new(hart_id: usize) -> Self {
         Guest { guest_id: hart_id }
+    }
+
+    /// Return guest dram space start
+    fn dram_base(&self) -> usize {
+        DRAM_BASE + DRAM_SIZE_PAR_HART * (self.guest_id + 1)
     }
 }
