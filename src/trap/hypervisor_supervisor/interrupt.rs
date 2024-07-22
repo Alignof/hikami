7ca382e4e025d1619ece9016c5ff2a3baef74885
Alingof <Alignof@outlook.com>
2024-07-22 22:06:48 +0900

    [fix] modify to refer guest data instead of `mhartid` register in hs-mode

--- src/trap/hypervisor_supervisor/interrupt.rs
+++ src/trap/hypervisor_supervisor/interrupt.rs
@@ -1,6 +1,5 @@
 use crate::h_extension::csrs::vsip;
 use crate::HYPERVISOR_DATA;
-use riscv::register::mhartid;
 use riscv::register::scause::Interrupt;
 
 /// Trap handler for Interrupt
@@ -8,15 +7,17 @@ pub unsafe fn trap_interrupt(interrupt_cause: Interrupt) {
     const CLINT_ADDR: usize = 0x200_0000;
     const MTIMECMP_ADDR: usize = 0x200_4000;
 
+    let guest = &HYPERVISOR_DATA.lock().guest;
+
     match interrupt_cause {
         Interrupt::SupervisorSoft => {
             vsip::set_ssoft();
-            let interrupt_addr = (CLINT_ADDR + mhartid::read() * 4) as *mut u64;
+            let interrupt_addr = (CLINT_ADDR + guest.hart_id() * 4) as *mut u64;
             interrupt_addr.write_volatile(0);
         }
         Interrupt::SupervisorTimer => {
             vsip::set_stimer();
-            let mtimecmp_addr = (MTIMECMP_ADDR + mhartid::read() * 8) as *mut u64;
+            let mtimecmp_addr = (MTIMECMP_ADDR + guest.hart_id() * 8) as *mut u64;
             mtimecmp_addr.write_volatile(u64::MAX);
         }
         Interrupt::SupervisorExternal => riscv::asm::wfi(), // wait for interrupt
@@ -24,5 +25,5 @@ pub unsafe fn trap_interrupt(interrupt_cause: Interrupt) {
     }
 
     // restore context data
-    HYPERVISOR_DATA.lock().context.load();
+    guest.context.load();
 }
