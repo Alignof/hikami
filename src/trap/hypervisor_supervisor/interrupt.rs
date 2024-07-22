22918bb1ca2d6addcfdf94874777a288ee2cafde
Alingof <Alignof@outlook.com>
2024-07-22 23:10:25 +0900

    [update] update trapping `SupervisorTimer`

--- src/trap/hypervisor_supervisor/interrupt.rs
+++ src/trap/hypervisor_supervisor/interrupt.rs
@@ -1,11 +1,11 @@
-use crate::h_extension::csrs::vsip;
+use crate::h_extension::csrs::{hvip, vsip, InterruptKind};
 use crate::HYPERVISOR_DATA;
 use riscv::register::scause::Interrupt;
+use riscv::register::sie;
 
 /// Trap handler for Interrupt
 pub unsafe fn trap_interrupt(interrupt_cause: Interrupt) {
     const CLINT_ADDR: usize = 0x200_0000;
-    const MTIMECMP_ADDR: usize = 0x200_4000;
 
     let guest = &HYPERVISOR_DATA.lock().guest;
 
@@ -16,9 +16,8 @@ pub unsafe fn trap_interrupt(interrupt_cause: Interrupt) {
             interrupt_addr.write_volatile(0);
         }
         Interrupt::SupervisorTimer => {
-            vsip::set_stimer();
-            let mtimecmp_addr = (MTIMECMP_ADDR + guest.hart_id() * 8) as *mut u64;
-            mtimecmp_addr.write_volatile(u64::MAX);
+            hvip::set(InterruptKind::Vsti);
+            sie::clear_stimer();
         }
         Interrupt::SupervisorExternal => riscv::asm::wfi(), // wait for interrupt
         _ => panic!("unknown interrupt type"),
