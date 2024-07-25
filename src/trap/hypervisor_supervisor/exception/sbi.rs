cbb377963e1bec5abc8076462cb10c8a0b1caf1e
Alingof <Alignof@outlook.com>
2024-07-25 23:34:18 +0900

    [add] add `sbi_base_handler` to exception/sbi.rs

--- src/trap/hypervisor_supervisor/exception/sbi.rs
+++ src/trap/hypervisor_supervisor/exception/sbi.rs
@@ -1,17 +1,27 @@
-use crate::guest;
+//! Handle VS-mode Ecall exception
+//! See [https://github.com/riscv-non-isa/riscv-sbi-doc/releases/download/v2.0/riscv-sbi.pdf](https://github.com/riscv-non-isa/riscv-sbi-doc/releases/download/v2.0/riscv-sbi.pdf)
 
-pub fn sbi_base_handler(func_id: usize, context: &guest::context::Context) -> usize {
+use sbi_rt::SbiRet;
+
+/// sbi ecall handler for Base Extension (EID: #0x10)
+///
+/// All functions in the base extension must be supported by all SBI implementations,
+/// so there are no error returns defined. (p.13)
+pub fn sbi_base_handler(func_id: usize) -> SbiRet {
     use sbi_spec::base::*;
-    match func_id {
+    let result_value = match func_id {
         GET_SBI_SPEC_VERSION => sbi_rt::get_sbi_impl_version(),
         GET_SBI_IMPL_ID => sbi_rt::get_sbi_impl_id(),
         GET_SBI_IMPL_VERSION => sbi_rt::get_sbi_impl_version(),
-        PROBE_EXTENSION => {
-            let extension = sbi_rt::get_sbi_impl_version();
-            sbi_rt::probe_extension(extension).raw
-        }
+        PROBE_EXTENSION => sbi_rt::probe_extension(sbi_rt::Base).raw,
         GET_MVENDORID => sbi_rt::get_mvendorid(),
         GET_MIMPID => sbi_rt::get_mimpid(),
         GET_MARCHID => sbi_rt::get_marchid(),
+        _ => unreachable!(),
+    };
+
+    SbiRet {
+        error: 0, // no error returns
+        value: result_value,
     }
 }
