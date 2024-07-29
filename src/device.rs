1277659f5438f4eef3340f142c23850324b59c0c
Alingof <Alignof@outlook.com>
2024-07-29 14:24:19 +0900

    [update] remove dtb data from `Devices`

--- src/device.rs
+++ src/device.rs
@@ -5,9 +5,9 @@ mod plic;
 mod uart;
 mod virtio;
 
-use crate::memmap::DeviceMemmap;
 use crate::HypervisorData;
 use alloc::vec::Vec;
+use fdt::Fdt;
 
 /// Manage devices sush as uart, plic, etc...
 ///
@@ -15,8 +15,6 @@ use alloc::vec::Vec;
 /// Each devices **must** be implemented Device trait.
 #[derive(Debug)]
 pub struct Devices {
-    dtb_addr: usize,
-    dtb_size: usize,
     uart: uart::Uart,
     virtio: Vec<virtio::VirtIO>,
     initrd: initrd::Initrd,
@@ -24,21 +22,12 @@ pub struct Devices {
     plic_context: usize,
 }
 
-impl Devices {
-    /// Get dtb data.
-    fn dtb_data(&self) -> (usize, usize) {
-        (self.dtb_addr, self.dtb_size)
-    }
-}
-
 impl HypervisorData {
     /// Set device data.
     ///
     /// It replace None (uninit value) to Some(init_device).
-    pub fn init_devices(&mut self, dtb_addr: usize, dtb_size: usize, memory_map: DeviceMemmap) {
+    pub fn init_devices(&mut self, device_tree: Fdt) {
         self.devices.replace(Devices {
-            dtb_addr,
-            dtb_size,
             uart: uart::Uart::new(&device_tree, "/soc/serial"),
             virtio: virtio::VirtIO::new_all(&device_tree, "/soc/virtio_mmio"),
             initrd: initrd::Initrd::new(&device_tree, "/chosen"),
