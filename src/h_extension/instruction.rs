f950ba531daa7540c8e0f98412149409ee718c80
Alingof <Alignof@outlook.com>
2024-09-03 02:41:10 +0900

    [doc] add doc comment to `h_extension` module

--- src/h_extension/instruction.rs
+++ src/h_extension/instruction.rs
@@ -1,5 +1,8 @@
+//! Utility for H extension instructions.
+
 use core::arch::asm;
 
+/// Hypervisor memory management fence for all virtual machines and guest physical addresses.
 #[inline(always)]
 #[allow(clippy::inline_always)]
 pub fn hfence_gvma_all() {
