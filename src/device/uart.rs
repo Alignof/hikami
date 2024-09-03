4ebbf9c173282b154c1dc630f3bf37c020dbca0d
Alingof <Alignof@outlook.com>
2024-09-03 13:42:03 +0900

    [wip][update] remove util.rs and move its contant to device/uart.rs

--- src/device/uart.rs
+++ src/device/uart.rs
@@ -2,6 +2,8 @@
 
 use super::{Device, PTE_FLAGS_FOR_DEVICE};
 use crate::memmap::{GuestPhysicalAddress, HostPhysicalAddress, MemoryMap};
+
+use core::fmt::{self, Write};
 use fdt::Fdt;
 use rustsbi::{Physical, SbiRet};
 
@@ -10,6 +12,42 @@ mod register {
 
     /// LSR register offset.
     pub const LSR_OFFSET: usize = 3;
+}
+
+/// Print to standard output.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::util::_print(format_args!($($arg)*)));
+}
+
+/// Print with linebreak to standard output.
+#[macro_export]
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+/// Print function calling from print macro
+pub fn _print(args: fmt::Arguments) {
+    let mut writer = UartWriter {};
+    writer.write_fmt(args).unwrap();
+}
+
+struct UartWriter;
+
+impl Write for UartWriter {
+    /// Write string to tty via UART.
+    #[allow(clippy::cast_possible_wrap)]
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let uart_addr = UART_ADDR.raw() as *mut u32;
+        for c in s.bytes() {
+            unsafe {
+                while (uart_addr.read_volatile() as i32) < 0 {}
+                uart_addr.write_volatile(u32::from(c));
+            }
+        }
+        Ok(())
+    }
 }
 
 /// UART: Universal asynchronous receiver-transmitter
