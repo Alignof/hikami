ad3b29d1ff4b2c98ca0f6d91778b06110ba6296a
Alingof <Alignof@outlook.com>
2024-09-05 19:36:26 +0900

    [update] remove `PlicEmulateError::InvalidEnableId`

--- src/trap/hypervisor_supervisor/exception.rs
+++ src/trap/hypervisor_supervisor/exception.rs
@@ -122,7 +122,6 @@ pub unsafe fn trap_exception(exception_cause: Exception) -> ! {
                     }
                     Err(
                         PlicEmulateError::InvalidAddress
-                        | PlicEmulateError::InvalidEnableId
                         | PlicEmulateError::InvalidContextId
                         | PlicEmulateError::ReservedRegister,
                     ) => hs_forward_exception(),
@@ -159,7 +158,6 @@ pub unsafe fn trap_exception(exception_cause: Exception) -> ! {
                     }
                     Err(
                         PlicEmulateError::InvalidAddress
-                        | PlicEmulateError::InvalidEnableId
                         | PlicEmulateError::InvalidContextId
                         | PlicEmulateError::ReservedRegister,
                     ) => hs_forward_exception(),
