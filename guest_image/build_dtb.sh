e044c3455780cc9845cc6374044a98ea792ad296
Alingof <Alignof@outlook.com>
2024-09-10 12:36:31 +0900

    [update] update creating dts command in build_dtb.sh

--- guest_image/build_dtb.sh
+++ guest_image/build_dtb.sh
@@ -12,7 +12,15 @@ fi
 if [ "$#" -eq 1 ]; then
     case "$1" in
         "create")
-            qemu-system-riscv64 -S -gdb tcp::10000 -machine virt -bios none  -m 256M -machine dumpdtb=qemu.dtb
+            qemu-system-riscv64 -S -gdb tcp::10000 \
+                -machine virt \
+                -bios none  \
+                -m 256M \
+                -drive file=../rootfs.img,format=raw,id=hd0 \
+                -device virtio-blk-device,drive=hd0 \
+                -append "root=/dev/vda rw console=ttyS0" \
+                -kernel ../target/riscv64imac-unknown-none-elf/debug/hikami \
+                -machine dumpdtb=qemu.dtb
             dtc -I dtb -O dts -o guest.dts qemu.dtb
             rm -f qemu.dtb
             ;;
