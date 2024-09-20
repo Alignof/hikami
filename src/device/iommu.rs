419bcc36ad55971d2fdeec82177327e20136f82c
Alingof <Alignof@outlook.com>
2024-09-20 19:16:19 +0900

    [update] remove `iommu::constants` module

--- src/device/iommu.rs
+++ src/device/iommu.rs
@@ -10,61 +10,6 @@ use register_map::IoMmuRegisters;
 
 use fdt::Fdt;
 
-mod constants {
-    //! Constants for IOMMU.
-
-    /// A read-only register reporting features supported by the IOMMU.
-    pub const REG_CAPABILITIES: usize = 0x0;
-    /// Page-based 39-bit virtual addressing is supported.
-    pub const FIELD_CAPABILITIES_SV39X4: usize = 17;
-
-    /// Queue entry size
-    /// N = 4096 / 16 = 256
-    pub const QUEUE_ENTRY_NUM: usize = 256;
-    /// Queue ppn
-    /// B = log_2(N) = 8
-    pub const QUEUE_PPN: usize = 8;
-
-    /// Command-queue base
-    pub const REG_CQB: usize = 0x18;
-    /// Holds the number of entries in command-queue as a log to base 2 minus 1.
-    pub const FIELD_CQB_LOG2SZ: usize = 0;
-    /// Holds the PPN of the root page of the in-memory command-queue used by software to queue commands to the IOMMU.
-    pub const FIELD_CQB_PPN: usize = 10;
-    /// Command-queue tail
-    pub const REG_CQT: usize = 0x24;
-    /// Command-queue CSR
-    pub const REG_CQCSR: usize = 0x48;
-    /// The command-queue is active if cqon is 1.
-    pub const FIELD_CQCSR_CQON: usize = 0x10;
-
-    /// Fault-queue base
-    pub const REG_FQB: usize = 0x28;
-    /// Holds the number of entries in fault-queue as a log to base 2 minus 1.
-    pub const FIELD_FQB_LOG2SZ: usize = 0;
-    /// Holds the PPN of the root page of the in-memory fault-queue used by software to queue faults to the IOMMU.
-    pub const FIELD_FQB_PPN: usize = 10;
-    /// Fault-queue tail
-    pub const REG_FQT: usize = 0x34;
-    /// Fault-queue CSR
-    pub const REG_FQCSR: usize = 0x4c;
-    /// The fault-queue is active if cqon is 1.
-    pub const FIELD_FQCSR_FQON: usize = 0x10;
-
-    /// Page-request-queue base
-    pub const REG_PQB: usize = 0x18;
-    /// Holds the number of entries in page-request-queue as a log to base 2 minus 1.
-    pub const FIELD_PQB_LOG2SZ: usize = 0;
-    /// Holds the PPN of the root page of the in-memory page-request-queue used by software to queue page-requests to the IOMMU.
-    pub const FIELD_PQB_PPN: usize = 10;
-    /// Page-request-queue tail
-    pub const REG_PQT: usize = 0x24;
-    /// Page-request-queue CSR
-    pub const REG_PQCSR: usize = 0x48;
-    /// The page-request-queue is active if cqon is 1.
-    pub const FIELD_PQCSR_PQON: usize = 0x10;
-}
-
 /// For `ddtp.iommu_mode`.
 enum IoMmuMode {
     /// No inbound memory transactions are allowed by the IOMMU.
@@ -102,7 +47,7 @@ impl Device for IoMmu {
 
         // 1. Read the capabilities register to discover the capabilities of the IOMMU.
         // 2. Stop and report failure if capabilities.version is not supported.
-        let (major, minor) = registers.capabilities.version();
+        let (major, _minor) = registers.capabilities.version();
         assert!(major >= 1);
         assert!(registers.capabilities.is_sv39x4_supported());
 
