7e7111653e05d09494583b44e03d682c0db9474c
Alingof <Alignof@outlook.com>
2024-10-06 17:21:33 +0900

    [!][update] remove `ShadowStack` struct (shadow stack is allocated by guest os)

--- src/emulate_extension/zicfiss.rs
+++ src/emulate_extension/zicfiss.rs
@@ -2,9 +2,6 @@
 //! Ref: [https://github.com/riscv/riscv-cfi/releases/download/v1.0/riscv-cfi.pdf](https://github.com/riscv/riscv-cfi/releases/download/v1.0/riscv-cfi.pdf)
 
 use super::{pseudo_vs_exception, CsrData};
-use crate::memmap::page_table::constants::PAGE_SIZE;
-use crate::memmap::HostPhysicalAddress;
-use crate::PageBlock;
 use crate::HYPERVISOR_DATA;
 
 use core::cell::OnceCell;
@@ -20,59 +17,8 @@ const SOFTWARE_CHECK_EXCEPTION: usize = 18;
 /// Shadow stack fault. (tval value)
 const SHADOW_STACK_FAULT: usize = 3;
 
-/// Shadow Stack
-struct ShadowStack {
-    top: *const usize,
-    bottom: *const usize,
-    stack_pointer: *mut usize,
-}
-
-impl ShadowStack {
-    /// Allocate memory region for shadow stack.
-    pub fn new() -> Self {
-        let stack_addr = PageBlock::alloc();
-        let base_ptr = stack_addr.0 as *const usize;
-        ShadowStack {
-            top: unsafe { base_ptr.byte_add(PAGE_SIZE) },
-            bottom: base_ptr,
-            stack_pointer: unsafe { base_ptr.cast_mut().byte_add(PAGE_SIZE) },
-        }
-    }
-
-    /// Push value to shadow stack
-    pub fn push(&mut self, value: usize) {
-        unsafe {
-            self.stack_pointer = self.stack_pointer.byte_sub(core::mem::size_of::<usize>());
-            if self.stack_pointer.cast_const() < self.bottom {
-                panic!("stack smashed!");
-            }
-            self.stack_pointer.write_volatile(value);
-        }
-    }
-
-    /// Pop value from shadow stack
-    pub fn pop(&mut self) -> usize {
-        unsafe {
-            let pop_value = self.stack_pointer.read_volatile();
-            self.stack_pointer = self.stack_pointer.byte_add(core::mem::size_of::<usize>());
-            if self.stack_pointer.cast_const() > self.top {
-                panic!("stack smashed!");
-            }
-
-            pop_value
-        }
-    }
-
-    pub fn get_ssp(&self) -> HostPhysicalAddress {
-        HostPhysicalAddress(self.stack_pointer as usize)
-    }
-}
-
 /// Singleton for Zicfiss extension
 pub struct Zicfiss {
-    /// Shadow stack
-    pub shadow_stack: ShadowStack,
-
     /// Shadow stack pointer
     pub ssp: CsrData,
 
@@ -86,10 +32,86 @@ pub struct Zicfiss {
 impl Zicfiss {
     pub fn new() -> Self {
         Zicfiss {
-            shadow_stack: ShadowStack::new(),
             ssp: CsrData(0),
             sse: false,
         }
+    }
+
+    fn ssp_ptr(&self) -> *mut usize {
+        self.ssp.0 as *mut usize
+    }
+
+    /// Push value to shadow stack
+    pub fn ss_push(&mut self, value: usize) {
+        unsafe {
+            self.ssp = CsrData(self.ssp_ptr().byte_sub(core::mem::size_of::<usize>()) as u64);
+            self.ssp_ptr().write_volatile(value);
+        }
+    }
+
+    /// Pop value from shadow stack
+    pub fn ss_pop(&mut self) -> usize {
+        unsafe {
+            let pop_value = self.ssp_ptr().read_volatile();
+            self.ssp = CsrData(self.ssp_ptr().byte_add(core::mem::size_of::<usize>()) as u64);
+
+            pop_value
+        }
+    }
+}
+
+/// Emulate Zicfiss instruction.
+pub fn instruction(inst: Instruction) {
+    let hypervisor_data = unsafe { HYPERVISOR_DATA.lock() };
+    let mut context = hypervisor_data.get().unwrap().guest().context;
+    unsafe { ZICFISS_DATA.lock().get_or_init(|| Zicfiss::new()) };
+    let mut zicfiss_data = unsafe { ZICFISS_DATA.lock() };
+    let zicfiss = zicfiss_data.get_mut().unwrap();
+
+    match inst.opc {
+        OpcodeKind::Zicfiss(ZicfissOpcode::SSPUSH) => {
+            if zicfiss.sse {
+                let push_value = context.xreg(inst.rs2.unwrap());
+                zicfiss.ss_push(push_value as usize);
+            }
+        }
+        OpcodeKind::Zicfiss(ZicfissOpcode::C_SSPUSH) => {
+            if zicfiss.sse {
+                let push_value = context.xreg(inst.rd.unwrap());
+                zicfiss.ss_push(push_value as usize);
+            }
+        }
+        OpcodeKind::Zicfiss(ZicfissOpcode::SSPOPCHK) => {
+            if zicfiss.sse {
+                let pop_value = zicfiss.ss_pop();
+                let expected_value = context.xreg(inst.rs1.unwrap()) as usize;
+                if pop_value != expected_value {
+                    drop(zicfiss_data);
+                    drop(hypervisor_data);
+                    pseudo_vs_exception(SOFTWARE_CHECK_EXCEPTION, SHADOW_STACK_FAULT)
+                }
+            }
+        }
+        OpcodeKind::Zicfiss(ZicfissOpcode::C_SSPOPCHK) => {
+            if zicfiss.sse {
+                let pop_value = zicfiss.ss_pop();
+                let expected_value = context.xreg(inst.rd.unwrap()) as usize;
+                if pop_value != expected_value {
+                    drop(zicfiss_data);
+                    drop(hypervisor_data);
+                    pseudo_vs_exception(SOFTWARE_CHECK_EXCEPTION, SHADOW_STACK_FAULT)
+                }
+            }
+        }
+        OpcodeKind::Zicfiss(ZicfissOpcode::SSRDP) => {
+            if zicfiss.sse {
+                context.set_xreg(inst.rd.unwrap(), zicfiss.ssp.0 as u64);
+            } else {
+                context.set_xreg(inst.rd.unwrap(), 0);
+            }
+        }
+        OpcodeKind::Zicfiss(ZicfissOpcode::SSAMOSWAP_W | ZicfissOpcode::SSAMOSWAP_D) => todo!(),
+        _ => todo!(),
     }
 }
 
@@ -137,61 +159,5 @@ pub fn csrs(inst: Instruction) {
         unsupported_csr_num => {
             unimplemented!("unsupported CSRs: {unsupported_csr_num:#x}")
         }
-    }
-}
-
-/// Emulate Zicfiss instruction.
-pub fn instruction(inst: Instruction) {
-    let hypervisor_data = unsafe { HYPERVISOR_DATA.lock() };
-    let mut context = hypervisor_data.get().unwrap().guest().context;
-    unsafe { ZICFISS_DATA.lock().get_or_init(|| Zicfiss::new()) };
-    let mut zicfiss_data = unsafe { ZICFISS_DATA.lock() };
-    let zicfiss = zicfiss_data.get_mut().unwrap();
-
-    match inst.opc {
-        OpcodeKind::Zicfiss(ZicfissOpcode::SSPUSH) => {
-            if zicfiss.sse {
-                let push_value = context.xreg(inst.rs2.unwrap());
-                zicfiss.shadow_stack.push(push_value as usize);
-            }
-        }
-        OpcodeKind::Zicfiss(ZicfissOpcode::C_SSPUSH) => {
-            if zicfiss.sse {
-                let push_value = context.xreg(inst.rd.unwrap());
-                zicfiss.shadow_stack.push(push_value as usize);
-            }
-        }
-        OpcodeKind::Zicfiss(ZicfissOpcode::SSPOPCHK) => {
-            if zicfiss.sse {
-                let pop_value = zicfiss.shadow_stack.pop();
-                let expected_value = context.xreg(inst.rs1.unwrap()) as usize;
-                if pop_value != expected_value {
-                    drop(zicfiss_data);
-                    drop(hypervisor_data);
-                    pseudo_vs_exception(SOFTWARE_CHECK_EXCEPTION, SHADOW_STACK_FAULT)
-                }
-            }
-        }
-        OpcodeKind::Zicfiss(ZicfissOpcode::C_SSPOPCHK) => {
-            if zicfiss.sse {
-                let pop_value = zicfiss.shadow_stack.pop();
-                let expected_value = context.xreg(inst.rd.unwrap()) as usize;
-                if pop_value != expected_value {
-                    drop(zicfiss_data);
-                    drop(hypervisor_data);
-                    pseudo_vs_exception(SOFTWARE_CHECK_EXCEPTION, SHADOW_STACK_FAULT)
-                }
-            }
-        }
-        OpcodeKind::Zicfiss(ZicfissOpcode::SSRDP) => {
-            if zicfiss.sse {
-                let ssp = zicfiss.shadow_stack.get_ssp();
-                context.set_xreg(inst.rd.unwrap(), ssp.0 as u64);
-            } else {
-                context.set_xreg(inst.rd.unwrap(), 0);
-            }
-        }
-        OpcodeKind::Zicfiss(ZicfissOpcode::SSAMOSWAP_W | ZicfissOpcode::SSAMOSWAP_D) => todo!(),
-        _ => todo!(),
     }
 }
