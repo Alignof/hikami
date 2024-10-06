f1d78e59a71fae788c43a03e60607cd87490bec4
Alingof <Alignof@outlook.com>
2024-10-06 16:39:52 +0900

    [fix] fix updating priv mode in `pseudo_vs_exception`

--- src/emulate_extension.rs
+++ src/emulate_extension.rs
@@ -7,11 +7,12 @@ use crate::trap::hypervisor_supervisor::hstrap_exit;
 use crate::HYPERVISOR_DATA;
 
 use core::arch::asm;
+use riscv::register::sstatus;
 
 /// Throw an VS-level exception.
 /// * `exception_num`: Exception number. (store to vscause)
 /// * `trap_value`: Trap value. (store to vstval)
-pub fn pseudo_vs_exception(exception_num: usize, trap_value: usize) {
+pub fn pseudo_vs_exception(exception_num: usize, trap_value: usize) -> ! {
     unsafe {
         let hypervisor_data = HYPERVISOR_DATA.lock();
         let mut context = hypervisor_data.get().unwrap().guest().context;
@@ -23,6 +24,15 @@ pub fn pseudo_vs_exception(exception_num: usize, trap_value: usize) {
             cause = in(reg) exception_num,
             tval = in(reg) trap_value,
         );
+
+        let spp = sstatus::read().spp();
+        let vsstatus: usize;
+        asm!("csrr {status}, vsstatus", status = out(reg) vsstatus);
+        asm!(
+            "csrw vsstatus, {status}",
+            status = in(reg) (vsstatus & !(1 << 8)) | (spp as usize) << 8
+        );
+        context.set_sstatus(context.sstatus() | 1 << 8);
 
         context.set_sepc(vstvec::read().bits());
 
