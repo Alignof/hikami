d5bd4840b3e6ff8d1377f401696206dacf0b8983
Alingof <Alignof@outlook.com>
2025-01-12 14:40:41 +0900

    [add] add `Sata::storing_base_addr`

--- src/device/pci/sata.rs
+++ src/device/pci/sata.rs
@@ -55,6 +55,57 @@ impl Sata {
         }
     }
 
+    /// Emulate storing base address to `CLB` of `FB`
+    fn storing_base_addr(
+        &mut self,
+        dst_addr: HostPhysicalAddress,
+        offset: usize,
+        port_offset: usize,
+        value: u32,
+    ) -> Result<(), DeviceEmulateError> {
+        let base_addr = self.abar.start;
+        let cmd_list_gpa = if port_offset == 0x0 {
+            let upper_addr = unsafe {
+                core::ptr::read_volatile((base_addr.raw() + offset + 0x4) as *const u32) as usize
+            };
+            GuestPhysicalAddress(upper_addr << 32 | value as usize)
+        } else {
+            let lower_addr = unsafe {
+                core::ptr::read_volatile((base_addr.raw() + offset) as *const u32) as usize
+            };
+            GuestPhysicalAddress((value as usize) << 32 | lower_addr)
+        };
+        if (0x9000_0000..0xa000_0000).contains(&cmd_list_gpa.raw()) {
+            if let Ok(cmd_list_hpa) = g_stage_trans_addr(cmd_list_gpa) {
+                crate::println!(
+                    "[translate] P{}CLB: {:#x}(GPA) -> {:#x}(HPA)",
+                    (offset - 0x100) / 0x80,
+                    cmd_list_gpa.raw(),
+                    cmd_list_hpa.raw()
+                );
+
+                let lower_offset = offset & !0xb111;
+                unsafe {
+                    core::ptr::write_volatile(
+                        (base_addr.raw() + lower_offset) as *mut u32,
+                        (cmd_list_hpa.raw() & 0xffff_ffff) as u32,
+                    );
+                    core::ptr::write_volatile(
+                        (base_addr.raw() + lower_offset + 4) as *mut u32,
+                        (cmd_list_hpa.raw() >> 32 & 0xffff_ffff) as u32,
+                    );
+                }
+
+                return Ok(());
+            }
+        }
+
+        // pass through as if it is in the middle of a translation
+        self.pass_through_storing(dst_addr, value);
+
+        Ok(())
+    }
+
     /// Emulate storing HBA Memory Registers.
     pub fn emulate_storing(
         &mut self,
@@ -78,43 +129,12 @@ impl Sata {
                 // 0x00: command list base address, 1K-byte aligned
                 // 0x04: command list base address upper 32 bits
                 port_offset @ (0x00 | 0x04) => {
-                    let cmd_list_gpa = if port_offset == 0x0 {
-                        let upper_addr = unsafe {
-                            core::ptr::read_volatile((base_addr.raw() + offset + 0x4) as *const u32)
-                                as usize
-                        };
-                        GuestPhysicalAddress(upper_addr << 32 | value as usize)
-                    } else {
-                        let lower_addr = unsafe {
-                            core::ptr::read_volatile((base_addr.raw() + offset) as *const u32)
-                                as usize
-                        };
-                        GuestPhysicalAddress((value as usize) << 32 | lower_addr)
-                    };
-                    if (0x9000_0000..0xa000_0000).contains(&cmd_list_gpa.raw()) {
-                        if let Ok(cmd_list_hpa) = g_stage_trans_addr(cmd_list_gpa) {
-                            crate::println!(
-                                "[translate] {:#x}(GPA) -> {:#x}(HPA)",
-                                cmd_list_gpa.raw(),
-                                cmd_list_hpa.raw()
-                            );
-                            unsafe {
-                                core::ptr::write_volatile(
-                                    (base_addr.raw() + offset) as *mut u32,
-                                    (cmd_list_hpa.raw() & 0xffff_ffff) as u32,
-                                );
-                                core::ptr::write_volatile(
-                                    (base_addr.raw() + offset + 4) as *mut u32,
-                                    (cmd_list_hpa.raw() >> 32 & 0xffff_ffff) as u32,
-                                );
-                            }
-
-                            return Ok(());
-                        }
-                    }
-
-                    // pass through as if it is in the middle of a translation
-                    self.pass_through_storing(dst_addr, value);
+                    self.storing_base_addr(dst_addr, offset, port_offset, value)?
+                }
+                // command issue
+                0x38 => {
+                    crate::println!("[command issue] {}", value.trailing_zeros());
+                    crate::println!("[command issue] count one {}", value.count_ones());
                 }
                 // other registers
                 _ => self.pass_through_storing(dst_addr, value),
