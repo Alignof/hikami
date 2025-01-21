32856eb0902a6dc39e1030953aff9dceb1a929ed
Alingof <Alignof@outlook.com>
2025-01-22 01:31:16 +0900

    [add] implement restoring translated address

--- src/device/axi_sdc.rs
+++ src/device/axi_sdc.rs
@@ -65,6 +65,8 @@ impl EmulateDevice for Mmc {
         let offset = dst_addr.raw() - self.base_addr.raw();
         match offset {
             // Command
+            //
+            // Start transfer when write command to `Command`
             4 => {
                 let registers_ptr = self.base_addr.raw() as *mut SdcRegisters;
                 unsafe {
@@ -107,15 +109,48 @@ impl EmulateDevice for Mmc {
                         self.dma_alt_buffer = new_heap;
                     }
                 }
-                Self::pass_through_storing(dst_addr, value as u32)
+                Self::pass_through_storing(dst_addr, value)
+            }
+            // Command interrupt enable
+            //
+            // End transfer if write zero to it
+            52 => {
+                let registers_ptr = self.base_addr.raw() as *mut SdcRegisters;
+                // restore address
+                unsafe {
+                    (*registers_ptr).dma_addres = self.dma_addr.raw() as u64;
+                }
+
+                if self.dma_alt_buffer.len() > 0 {
+                    unsafe {
+                        if (*registers_ptr).command >> 5 & 0x1 == 1 {
+                            // write back data to guest memory if command is `read`
+                            let heap_ptr = self.dma_alt_buffer.as_ptr().cast_mut();
+                            for offset in (0..self.dma_alt_buffer.len()).step_by(PAGE_SIZE) {
+                                let dst_gpa = self.dma_addr + offset;
+                                let dst_hpa = g_stage_trans_addr(dst_gpa)
+                                    .expect("failed translation of data base address");
+
+                                core::ptr::copy(
+                                    heap_ptr.add(offset),
+                                    dst_hpa.raw() as *mut u8,
+                                    if offset + PAGE_SIZE < self.dma_alt_buffer.len() {
+                                        PAGE_SIZE
+                                    } else {
+                                        self.dma_alt_buffer.len() - offset
+                                    },
+                                );
+                            }
+                        }
+                    }
+                    self.dma_alt_buffer.clear();
+                }
             }
             // other registers
-            _ => Self::pass_through_storing(dst_addr, value as u32),
+            _ => Self::pass_through_storing(dst_addr, value),
         }
     }
 }
-
-impl EmulateDevice for Mmc {}
 
 impl MmioDevice for Mmc {
     fn new(_device_tree: &Fdt, _node_path: &str) -> Self {
--- src/device/axi_sdc/register.rs
+++ src/device/axi_sdc/register.rs
@@ -9,7 +9,7 @@ pub const REG_FIELD_SIZE: usize = core::mem::size_of::<SdcRegisters>();
 #[repr(C)]
 pub struct SdcRegisters {
     _argument: u32,
-    _command: u32,
+    pub command: u32,
     _response1: u32,
     _response2: u32,
     _response3: u32,
@@ -21,13 +21,13 @@ pub struct SdcRegisters {
     _software_reset: u32,
     _power_control: u32,
     _capability: u32,
-    _cmd_int_status: u32,
+    pub cmd_int_status: u32,
     _cmd_int_enable: u32,
     _dat_int_status: u32,
     _dat_int_enable: u32,
-    _block_size: u32,
-    _block_count: u32,
+    pub block_size: u32,
+    pub block_count: u32,
     _card_detect: u32,
     _reserved: [u32; 4],
-    dma_addres: u64,
+    pub dma_addres: u64,
 }
