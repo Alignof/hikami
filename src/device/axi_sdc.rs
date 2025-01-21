a4ca42d6ebb147aef483eff110c71115be14d4ed
Alingof <Alignof@outlook.com>
2025-01-21 23:43:43 +0900

    [add] implement `Mmc::emulate_storing`

--- src/device/axi_sdc.rs
+++ src/device/axi_sdc.rs
@@ -5,9 +5,12 @@
 mod register;
 
 use super::{EmulateDevice, MmioDevice, PTE_FLAGS_FOR_DEVICE};
+use crate::memmap::page_table::{constants::PAGE_SIZE, g_stage_trans_addr};
 use crate::memmap::{GuestPhysicalAddress, HostPhysicalAddress, MemoryMap};
-use fdt::Fdt;
 use register::{SdcRegisters, REG_FIELD_SIZE};
+
+use alloc::vec::Vec;
+use fdt::Fdt;
 
 #[allow(clippy::doc_markdown)]
 /// MMC: Multi Media Card
@@ -17,6 +20,10 @@ pub struct Mmc {
     base_addr: HostPhysicalAddress,
     /// Memory map size.
     size: usize,
+    /// DMA address.
+    dma_addr: GuestPhysicalAddress,
+    /// DMA alternative buffer
+    dma_alt_buffer: Vec<u8>,
 }
 
 impl Mmc {
@@ -31,11 +38,11 @@ impl Mmc {
         Some(Mmc {
             base_addr: HostPhysicalAddress(region.starting_address as usize),
             size: region.size.unwrap(),
+            dma_addr: GuestPhysicalAddress(0),
+            dma_alt_buffer: Vec::new(),
         })
     }
-}
 
-impl EmulateDevice for Mmc {
     /// Emulate loading port registers.
     #[allow(clippy::cast_possible_truncation)]
     fn emulate_loading(
@@ -52,20 +59,61 @@ impl EmulateDevice for Mmc {
     }
 
     /// Emulate storing port registers.
-    fn emulate_storing(
-        &mut self,
-        base_addr: HostPhysicalAddress,
-        dst_addr: HostPhysicalAddress,
-        value: u32,
-    ) {
-        let offset = dst_addr.raw() - base_addr.raw();
+    fn emulate_storing(&mut self, dst_addr: HostPhysicalAddress, value: u64) {
+        let offset = dst_addr.raw() - self.base_addr.raw();
         match offset {
-            0..REG_FIELD_SIZE => todo!(),
+            // Command
+            4 => {
+                let registers_ptr = self.base_addr.raw() as *mut SdcRegisters;
+                unsafe {
+                    let dma_buffer_size =
+                        ((*registers_ptr).block_size * (*registers_ptr).block_count) as usize;
+                    let dma_gpa = GuestPhysicalAddress((*registers_ptr).dma_addres as usize);
+                    self.dma_addr = dma_gpa;
+
+                    if dma_buffer_size <= PAGE_SIZE {
+                        // only translation
+                        let dma_hpa =
+                            g_stage_trans_addr(dma_gpa).expect("failed to translate dma address");
+                        (*registers_ptr).dma_addres = dma_hpa.raw() as u64;
+                    } else {
+                        // pass new buffer
+                        let mut new_heap = Vec::<u8>::with_capacity(dma_buffer_size);
+                        new_heap.set_len(dma_buffer_size);
+                        let new_heap_addr = new_heap.as_ptr() as usize;
+                        (*registers_ptr).dma_addres = new_heap_addr as u64;
+
+                        // write data to allocated memory if command is `write`
+                        if (value >> 6 & 1) == 1 {
+                            let heap_ptr = new_heap.as_ptr().cast_mut();
+                            for offset in (0..new_heap.len()).step_by(PAGE_SIZE) {
+                                let dst_gpa = dma_gpa + offset;
+                                let dst_hpa = g_stage_trans_addr(dst_gpa)
+                                    .expect("failed translation of data base address");
+
+                                core::ptr::copy(
+                                    dst_hpa.raw() as *const u8,
+                                    heap_ptr.add(offset),
+                                    if offset + PAGE_SIZE < new_heap.len() {
+                                        PAGE_SIZE
+                                    } else {
+                                        new_heap.len() - offset
+                                    },
+                                );
+                            }
+                        }
+                        self.dma_alt_buffer = new_heap;
+                    }
+                }
+                Self::pass_through_storing(dst_addr, value as u32)
+            }
             // other registers
-            _ => Self::pass_through_storing(dst_addr, value),
+            _ => Self::pass_through_storing(dst_addr, value as u32),
         }
     }
 }
+
+impl EmulateDevice for Mmc {}
 
 impl MmioDevice for Mmc {
     fn new(_device_tree: &Fdt, _node_path: &str) -> Self {
