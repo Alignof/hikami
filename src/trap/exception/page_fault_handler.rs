894e36b5952974ca40d9049e2696a257b69653c2
Alignof <alignof@outlook.com>
2025-01-25 02:31:30 +0900

    [!][fix] fix retrieving fault instruction if htinst is zero

--- src/trap/exception/page_fault_handler.rs
+++ src/trap/exception/page_fault_handler.rs
@@ -6,19 +6,24 @@
 use super::{hs_forward_exception, update_sepc_by_htinst_value};
 use crate::device::EmulateDevice;
 use crate::h_extension::csrs::{htinst, htval};
-use crate::memmap::page_table::g_stage_trans_addr;
-use crate::memmap::{GuestPhysicalAddress, HostPhysicalAddress};
+use crate::memmap::page_table::{g_stage_trans_addr, vs_stage_trans_addr};
+use crate::memmap::{GuestPhysicalAddress, GuestVirtualAddress, HostPhysicalAddress};
 use crate::HYPERVISOR_DATA;
 
 use raki::Instruction;
+use riscv::register::sepc;
 
 /// Fetch fault instruction
 fn fetch_fault_inst(fault_addr: HostPhysicalAddress) -> usize {
-    let inst_value = unsafe { (fault_addr.raw() as *const u32).read_volatile() };
-    if inst_value & 0b11 == 0b11 {
-        inst_value as usize
+    if fault_addr.raw() % 4 == 0 {
+        let inst_value = unsafe { (fault_addr.raw() as *const u32).read_volatile() };
+        if inst_value & 0b11 == 0b11 {
+            inst_value as usize
+        } else {
+            (inst_value & 0xffff) as usize
+        }
     } else {
-        (inst_value & 0xffff) as usize
+        unsafe { (fault_addr.raw() as *const u16).read_volatile() as usize }
     }
 }
 
@@ -31,8 +36,12 @@ pub fn load_guest_page_fault() {
     // thus it needed to flip bit 1.
     // ref: vol. II p.161
     let fault_inst = if fault_inst_value == 0 {
-        let fault_addr = g_stage_trans_addr(fault_addr).expect("not a identity map");
-        let fault_inst_value = fetch_fault_inst(fault_addr);
+        let fault_gva = GuestVirtualAddress(sepc::read());
+        let fault_gpa =
+            vs_stage_trans_addr(fault_gva).expect("failed to get a gpa of load fault instruction");
+        let fault_hpa =
+            g_stage_trans_addr(fault_gpa).expect("failed to get a hpa of load fault instruction");
+        let fault_inst_value = fetch_fault_inst(fault_hpa);
         assert_ne!(fault_inst_value, 0);
         Instruction::try_from(fault_inst_value).expect("decoding load fault instruction failed")
     } else {
@@ -88,8 +97,12 @@ pub fn store_guest_page_fault() {
     // thus it needed to flip bit 1.
     // ref: vol. II p.161
     let fault_inst = if fault_inst_value == 0 {
-        let fault_addr = g_stage_trans_addr(fault_addr).expect("not a identity map");
-        let fault_inst_value = fetch_fault_inst(fault_addr);
+        let fault_gva = GuestVirtualAddress(sepc::read());
+        let fault_gpa =
+            vs_stage_trans_addr(fault_gva).expect("failed to get a gpa of load fault instruction");
+        let fault_hpa =
+            g_stage_trans_addr(fault_gpa).expect("failed to get a hpa of load fault instruction");
+        let fault_inst_value = fetch_fault_inst(fault_hpa);
         assert_ne!(fault_inst_value, 0);
         Instruction::try_from(fault_inst_value).expect("decoding load fault instruction failed")
     } else {
