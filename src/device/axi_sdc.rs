a6b60b471fdb530f8f15f3c256e7762bf1c4481b
Alignof <alignof@outlook.com>
2025-01-25 20:33:48 +0900

    [!][fix] fix a trap register kind in `Mmc`

--- src/device/axi_sdc.rs
+++ src/device/axi_sdc.rs
@@ -43,58 +43,68 @@ impl EmulateDevice for Mmc {
     ) -> Result<(), DeviceEmulateError> {
         let offset = dst_addr.raw() - self.base_addr.raw();
         match offset {
-            // Command
+            // Argument
             //
-            // Start transfer when write command to `Command`
-            4 => {
+            // Start transfer when write command to `Argument`
+            // See: https://github.com/eugene-tarassov/vivado-risc-v/blob/d72a439f786b455cc321e2e615d7954a75f9ebde/sdc/axi_sdc_controller.v#L392
+            0 => {
                 let registers_ptr = self.base_addr.raw() as *mut SdcRegisters;
-                unsafe {
-                    let dma_block_size = ((*registers_ptr).block_size + 1) as usize;
-                    let dma_block_count = ((*registers_ptr).block_count + 1) as usize;
-                    let dma_buffer_size = dma_block_size * dma_block_count;
-                    let dma_gpa = GuestPhysicalAddress((*registers_ptr).dma_addres as usize);
-                    self.dma_addr = dma_gpa;
+                let command = unsafe { ((*registers_ptr).command) as usize };
+                let dma_block_count = unsafe { ((*registers_ptr).block_count + 1) as usize };
+                let dma_block_size = unsafe { ((*registers_ptr).block_size + 1) as usize };
+                let dma_gpa = GuestPhysicalAddress(unsafe { (*registers_ptr).dma_addres } as usize);
 
-                    if dma_buffer_size <= PAGE_SIZE {
-                        // only translation
-                        let dma_hpa =
-                            g_stage_trans_addr(dma_gpa).expect("failed to translate dma address");
-                        (*registers_ptr).dma_addres = dma_hpa.raw() as u64;
-                    } else {
-                        // pass new buffer
-                        let mut new_heap = Vec::<u8>::with_capacity(dma_buffer_size);
-                        new_heap.set_len(dma_buffer_size);
-                        let new_heap_addr = new_heap.as_ptr() as usize;
-                        (*registers_ptr).dma_addres = new_heap_addr as u64;
+                // if dma block count is zero, use response register instead of buffer.
+                if ((command >> 5) & 0b11) != 0b00 && dma_gpa != GuestPhysicalAddress(0) {
+                    // command with transfer
+                    unsafe {
+                        let dma_block_count = ((*registers_ptr).block_count + 1) as usize;
+                        let dma_block_size = ((*registers_ptr).block_size + 1) as usize;
+                        let dma_buffer_size = dma_block_size * dma_block_count;
+                        let dma_gpa = GuestPhysicalAddress((*registers_ptr).dma_addres as usize);
+                        self.dma_addr = dma_gpa;
 
-                        // write data to allocated memory if command is `write`
-                        if (value >> 6 & 1) == 1 {
-                            let heap_ptr = new_heap.as_ptr().cast_mut();
-                            for offset in (0..new_heap.len()).step_by(PAGE_SIZE) {
-                                let dst_gpa = dma_gpa + offset;
-                                let dst_hpa = g_stage_trans_addr(dst_gpa)
-                                    .expect("failed translation of data base address");
+                        if dma_buffer_size <= PAGE_SIZE {
+                            // only translation
+                            let dma_hpa = g_stage_trans_addr(dma_gpa)
+                                .expect("failed to translate dma address");
+                            (*registers_ptr).dma_addres = dma_hpa.raw() as u64;
+                        } else {
+                            // pass new buffer
+                            let mut new_heap = Vec::<u8>::with_capacity(dma_buffer_size);
+                            new_heap.set_len(dma_buffer_size);
+                            let new_heap_addr = new_heap.as_ptr() as usize;
+                            (*registers_ptr).dma_addres = new_heap_addr as u64;
 
-                                core::ptr::copy(
-                                    dst_hpa.raw() as *const u8,
-                                    heap_ptr.add(offset),
-                                    if offset + PAGE_SIZE < new_heap.len() {
-                                        PAGE_SIZE
-                                    } else {
-                                        new_heap.len() - offset
-                                    },
-                                );
+                            // write data to allocated memory if command is `write`
+                            if ((command >> 6) & 1) == 1 {
+                                let heap_ptr = new_heap.as_ptr().cast_mut();
+                                for offset in (0..new_heap.len()).step_by(PAGE_SIZE) {
+                                    let dst_gpa = dma_gpa + offset;
+                                    let dst_hpa = g_stage_trans_addr(dst_gpa)
+                                        .expect("failed translation of data base address");
+
+                                    core::ptr::copy(
+                                        dst_hpa.raw() as *const u8,
+                                        heap_ptr.add(offset),
+                                        if offset + PAGE_SIZE < new_heap.len() {
+                                            PAGE_SIZE
+                                        } else {
+                                            new_heap.len() - offset
+                                        },
+                                    );
+                                }
                             }
+                            self.dma_alt_buffer = new_heap;
                         }
-                        self.dma_alt_buffer = new_heap;
                     }
                     self.is_transferring = true;
                 }
             }
-            // Command interrupt status
+            // Data interrupt status
             //
             // End transfer if write zero to it
-            52 => {
+            60 => {
                 // end transfer
                 if value == 0 && self.is_transferring {
                     let registers_ptr = self.base_addr.raw() as *mut SdcRegisters;
