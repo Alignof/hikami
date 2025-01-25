0cf361885ecf489beb55a3566513e652433881cf
Alignof <alignof@outlook.com>
2025-01-25 14:54:33 +0900

    [!][fix] fix updating sepc when load/store guest page fault occured

--- src/trap/exception/page_fault_handler.rs
+++ src/trap/exception/page_fault_handler.rs
@@ -3,7 +3,7 @@
 //! - Load guest page fault
 //! - Store AMO guest page fault
 
-use super::{hs_forward_exception, update_sepc_by_htinst_value};
+use super::{hs_forward_exception, update_sepc_by_inst_type};
 use crate::device::EmulateDevice;
 use crate::h_extension::csrs::{htinst, htval};
 use crate::memmap::page_table::{g_stage_trans_addr, vs_stage_trans_addr};
@@ -31,11 +31,11 @@ fn fetch_fault_inst(fault_addr: HostPhysicalAddress) -> usize {
 pub fn load_guest_page_fault() {
     let fault_addr = GuestPhysicalAddress(htval::read().bits() << 2);
 
-    let fault_inst_value = htinst::read().bits();
+    let htinst_value = htinst::read().bits();
     // htinst bit 1 replaced with a 0.
     // thus it needed to flip bit 1.
     // ref: vol. II p.161
-    let fault_inst = if fault_inst_value == 0 {
+    let (fault_inst, is_compressed) = if htinst_value == 0 {
         let fault_gva = GuestVirtualAddress(sepc::read());
         let fault_gpa =
             vs_stage_trans_addr(fault_gva).expect("failed to get a gpa of load fault instruction");
@@ -43,10 +43,18 @@ pub fn load_guest_page_fault() {
             g_stage_trans_addr(fault_gpa).expect("failed to get a hpa of load fault instruction");
         let fault_inst_value = fetch_fault_inst(fault_hpa);
         assert_ne!(fault_inst_value, 0);
-        Instruction::try_from(fault_inst_value).expect("decoding load fault instruction failed")
+
+        (
+            Instruction::try_from(fault_inst_value)
+                .expect("decoding load fault instruction failed"),
+            fault_inst_value & 0b11 != 0b11,
+        )
     } else {
-        Instruction::try_from(fault_inst_value | 0b10)
-            .expect("decoding load fault instruction failed")
+        (
+            Instruction::try_from(htinst_value | 0b10)
+                .expect("decoding load fault instruction failed"),
+            (htinst_value & 0b10) >> 1 == 0,
+        )
     };
 
     let mut hypervisor_data = unsafe { HYPERVISOR_DATA.lock() };
@@ -59,7 +67,7 @@ pub fn load_guest_page_fault() {
     {
         let mut context = hypervisor_data.get().unwrap().guest().context;
         context.set_xreg(fault_inst.rd.expect("rd is not found"), u64::from(value));
-        update_sepc_by_htinst_value(fault_inst_value, &mut context);
+        update_sepc_by_inst_type(is_compressed, &mut context);
         return;
     }
 
@@ -68,7 +76,7 @@ pub fn load_guest_page_fault() {
             if let Ok(value) = sata.emulate_loading(HostPhysicalAddress(fault_addr.raw())) {
                 let mut context = hypervisor_data.get().unwrap().guest().context;
                 context.set_xreg(fault_inst.rd.expect("rd is not found"), u64::from(value));
-                update_sepc_by_htinst_value(fault_inst_value, &mut context);
+                update_sepc_by_inst_type(is_compressed, &mut context);
                 return;
             }
         }
@@ -78,7 +86,7 @@ pub fn load_guest_page_fault() {
         if let Ok(value) = mmc.emulate_loading(HostPhysicalAddress(fault_addr.raw())) {
             let mut context = hypervisor_data.get().unwrap().guest().context;
             context.set_xreg(fault_inst.rd.expect("rd is not found"), u64::from(value));
-            update_sepc_by_htinst_value(fault_inst_value, &mut context);
+            update_sepc_by_inst_type(is_compressed, &mut context);
             return;
         }
     }
@@ -92,11 +100,11 @@ pub fn load_guest_page_fault() {
 pub fn store_guest_page_fault() {
     let fault_addr = GuestPhysicalAddress(htval::read().bits() << 2);
 
-    let fault_inst_value = htinst::read().bits();
+    let htinst_value = htinst::read().bits();
     // htinst bit 1 replaced with a 0.
     // thus it needed to flip bit 1.
     // ref: vol. II p.161
-    let fault_inst = if fault_inst_value == 0 {
+    let (fault_inst, fault_inst_value, is_compressed) = if htinst_value == 0 {
         let fault_gva = GuestVirtualAddress(sepc::read());
         let fault_gpa =
             vs_stage_trans_addr(fault_gva).expect("failed to get a gpa of load fault instruction");
@@ -104,10 +112,20 @@ pub fn store_guest_page_fault() {
             g_stage_trans_addr(fault_gpa).expect("failed to get a hpa of load fault instruction");
         let fault_inst_value = fetch_fault_inst(fault_hpa);
         assert_ne!(fault_inst_value, 0);
-        Instruction::try_from(fault_inst_value).expect("decoding load fault instruction failed")
+
+        (
+            Instruction::try_from(fault_inst_value)
+                .expect("decoding store fault instruction failed"),
+            fault_inst_value,
+            (fault_inst_value & 0b11) != 0b11,
+        )
     } else {
-        Instruction::try_from(fault_inst_value | 0b10)
-            .expect("decoding load fault instruction failed")
+        (
+            Instruction::try_from(htinst_value | 0b10)
+                .expect("decoding store fault instruction failed"),
+            htinst_value,
+            (htinst_value & 0b10) >> 1 == 0,
+        )
     };
 
     let mut hypervisor_data = unsafe { HYPERVISOR_DATA.lock() };
@@ -125,7 +143,7 @@ pub fn store_guest_page_fault() {
         .plic
         .emulate_storing(HostPhysicalAddress(fault_addr.raw()), store_value as u32)
     {
-        update_sepc_by_htinst_value(fault_inst_value, &mut context);
+        update_sepc_by_inst_type(is_compressed, &mut context);
         return;
     }
 
@@ -134,7 +152,7 @@ pub fn store_guest_page_fault() {
             if let Ok(()) =
                 sata.emulate_storing(HostPhysicalAddress(fault_addr.raw()), store_value as u32)
             {
-                update_sepc_by_htinst_value(fault_inst_value, &mut context);
+                update_sepc_by_inst_type(is_compressed, &mut context);
                 return;
             }
         }
@@ -144,7 +162,7 @@ pub fn store_guest_page_fault() {
         if let Ok(()) =
             mmc.emulate_storing(HostPhysicalAddress(fault_addr.raw()), store_value as u32)
         {
-            update_sepc_by_htinst_value(fault_inst_value, &mut context);
+            update_sepc_by_inst_type(is_compressed, &mut context);
             return;
         }
     }
