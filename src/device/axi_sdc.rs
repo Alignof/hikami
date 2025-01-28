01d89566e20a76ff5b037d0f4ebcee6509dc7c9d
Alignof <alignof@outlook.com>
2025-01-28 14:52:22 +0900

    [update] change a type of `Mmc.dma_alt_buffer` to `DmaHostBuffer`

--- src/device/axi_sdc.rs
+++ src/device/axi_sdc.rs
@@ -4,12 +4,11 @@
 
 mod register;
 
-use super::{DeviceEmulateError, EmulateDevice, MmioDevice, PTE_FLAGS_FOR_DEVICE};
+use super::{DeviceEmulateError, DmaHostBuffer, EmulateDevice, MmioDevice, PTE_FLAGS_FOR_DEVICE};
 use crate::memmap::page_table::{constants::PAGE_SIZE, g_stage_trans_addr};
 use crate::memmap::{GuestPhysicalAddress, HostPhysicalAddress, MemoryMap};
 use register::SdcRegisters;
 
-use alloc::vec::Vec;
 use fdt::Fdt;
 
 #[allow(clippy::doc_markdown)]
@@ -23,7 +22,7 @@ pub struct Mmc {
     /// DMA address.
     dma_addr: GuestPhysicalAddress,
     /// DMA alternative buffer
-    dma_alt_buffer: Vec<u8>,
+    dma_alt_buffer: DmaHostBuffer,
     /// Is the mmc command being executed now.
     is_transferring: bool,
 }
@@ -68,31 +67,13 @@ impl EmulateDevice for Mmc {
                             (*registers_ptr).dma_addres = dma_hpa.raw() as u64;
                         } else {
                             // pass new buffer
-                            let mut new_heap = Vec::<u8>::with_capacity(dma_buffer_size);
-                            new_heap.set_len(dma_buffer_size);
-                            let new_heap_addr = new_heap.as_ptr() as usize;
-                            (*registers_ptr).dma_addres = new_heap_addr as u64;
+                            self.dma_alt_buffer.set_used_len(dma_buffer_size);
+                            (*registers_ptr).dma_addres = self.dma_alt_buffer.addr() as u64;
 
                             // write data to allocated memory if command is `write`
                             if ((command >> 6) & 1) == 1 {
-                                let heap_ptr = new_heap.as_ptr().cast_mut();
-                                for offset in (0..new_heap.len()).step_by(PAGE_SIZE) {
-                                    let dst_gpa = dma_gpa + offset;
-                                    let dst_hpa = g_stage_trans_addr(dst_gpa)
-                                        .expect("failed translation of data base address");
-
-                                    core::ptr::copy(
-                                        dst_hpa.raw() as *const u8,
-                                        heap_ptr.add(offset),
-                                        if offset + PAGE_SIZE < new_heap.len() {
-                                            PAGE_SIZE
-                                        } else {
-                                            new_heap.len() - offset
-                                        },
-                                    );
-                                }
+                                self.dma_alt_buffer.guest_to_host(dma_gpa);
                             }
-                            self.dma_alt_buffer = new_heap;
                         }
                     }
                     self.is_transferring = true;
@@ -110,29 +91,15 @@ impl EmulateDevice for Mmc {
                         (*registers_ptr).dma_addres = self.dma_addr.raw() as u64;
                     }
 
-                    if self.dma_alt_buffer.len() > 0 {
+                    // write back data to guest memory if command is `read`
+                    if self.dma_alt_buffer.is_used() {
                         unsafe {
-                            if (*registers_ptr).command >> 5 & 0x1 == 1 {
-                                // write back data to guest memory if command is `read`
-                                let heap_ptr = self.dma_alt_buffer.as_ptr().cast_mut();
-                                for offset in (0..self.dma_alt_buffer.len()).step_by(PAGE_SIZE) {
-                                    let dst_gpa = self.dma_addr + offset;
-                                    let dst_hpa = g_stage_trans_addr(dst_gpa)
-                                        .expect("failed translation of data base address");
-
-                                    core::ptr::copy(
-                                        heap_ptr.add(offset),
-                                        dst_hpa.raw() as *mut u8,
-                                        if offset + PAGE_SIZE < self.dma_alt_buffer.len() {
-                                            PAGE_SIZE
-                                        } else {
-                                            self.dma_alt_buffer.len() - offset
-                                        },
-                                    );
-                                }
+                            if (((*registers_ptr).command >> 5) & 0x1) == 1 {
+                                self.dma_alt_buffer.host_to_guest(self.dma_addr);
                             }
+
+                            self.dma_alt_buffer.clear_used_len();
                         }
-                        self.dma_alt_buffer.clear();
                     }
 
                     self.is_transferring = false;
@@ -159,7 +126,7 @@ impl MmioDevice for Mmc {
             base_addr: HostPhysicalAddress(region.starting_address as usize),
             size: region.size.unwrap(),
             dma_addr: GuestPhysicalAddress(0),
-            dma_alt_buffer: Vec::new(),
+            dma_alt_buffer: DmaHostBuffer::new(PAGE_SIZE),
             is_transferring: false,
         })
     }
