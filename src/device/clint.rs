f7d820c2c1be6480fce24bf75d9f29f93d53c84e
Alignof <alignof@outlook.com>
2025-02-04 16:45:17 +0900

    [!] remove `clint::register`

--- src/device/clint.rs
+++ src/device/clint.rs
@@ -4,23 +4,6 @@ use super::{MmioDevice, PTE_FLAGS_FOR_DEVICE};
 use crate::memmap::{GuestPhysicalAddress, HostPhysicalAddress, MemoryMap};
 use fdt::Fdt;
 
-mod register {
-    //! Ref: [https://chromitem-soc.readthedocs.io/en/latest/clint.html](https://chromitem-soc.readthedocs.io/en/latest/clint.html)
-    //!
-    //! | Register-Name | Offset(hex) | Size(Bits) | Reset(hex) | Description                                                        |
-    //! | ------------- | ----------- | ---------- | ---------- | -----------                                                        |
-    //! | msip          | 0x0         | 32         | 0x0        | This register generates machine mode software interrupts when set. |
-    //! | mtimecmp      | 0x4000      | 64         | 0x0        | This register holds the compare value for the timer.               |
-    //! | mtime         | 0xBFF8      | 64         | 0x0        | Provides the current timer value.                                  |
-
-    /// Offset of `MISP` register
-    pub const MSIP_OFFSET: usize = 0x0;
-    /// Offset of `MTIMECMP` register
-    pub const MTIMECMP_OFFSET: usize = 0x4000;
-    /// Offset of `MTIME` register
-    pub const MTIME_OFFSET: usize = 0xbff8;
-}
-
 #[allow(clippy::doc_markdown)]
 /// CLINT: Core Local INTerrupt
 /// Local interrupt controller
