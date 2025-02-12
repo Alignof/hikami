a292e33ac0f4e5b00c2efa88b076b2cb750380a4
Alingof <Alignof@outlook.com>
2025-02-12 19:09:03 +0900

    [add] init `extension_manager`

--- extension_manager/src/lib.rs
+++ extension_manager/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Hikami extension manager.
+//!
+//! It load extension emulation module as crate and expand function calls from macro.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+extern crate proc_macro;
+extern crate proc_macro2;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::Ident;
+
+include!(concat!(env!("OUT_DIR"), "/dependencies.rs"));
+
+/// Test: call `func` function in all of modules.
+#[proc_macro]
+pub fn call_all_funcs(_input: TokenStream) -> TokenStream {
+    let calls = CRATES.iter().map(|name| {
+        let name = name.trim().replace('-', "_");
+        let ident = Ident::new(&name, proc_macro2::Span::call_site());
+        quote! { #ident::func(); }
+    });
+
+    let expanded = quote! {
+        #(#calls)*
+    };
+
+    TokenStream::from(expanded)
 }
