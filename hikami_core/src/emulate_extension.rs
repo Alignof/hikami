a6a60def7af18278ea08827b205f34dd26572b81
Alingof <Alignof@outlook.com>
2025-07-03 01:00:11 +0900

    [add] add `is_csr_field_defined` to `EmulateExtension`

--- hikami_core/src/emulate_extension.rs
+++ hikami_core/src/emulate_extension.rs
@@ -18,6 +18,8 @@ pub trait EmulateExtension {
     fn csr_field(&mut self, inst: &Instruction, write_to_csr_value: u64, read_csr_value: &mut u64);
     /// Return whether given csr value defined in the extension.
     fn is_csr_defined(&self, csr_num: u16) -> bool;
+    /// Return whether given csr value has newly defined field.
+    fn is_csr_field_defined(&self, csr_num: u16) -> bool;
 }
 
 /// Holding a CSR value for CSRs emulation.
