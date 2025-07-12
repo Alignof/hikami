6dca9f02c00b09210093f168a4d16b87a4e4cf34
Alingof <Alignof@outlook.com>
2025-07-12 22:04:55 +0900

    [fix] change a memory map region if region size is less than the page size

--- hikami_core/src/device/clint.rs
+++ hikami_core/src/device/clint.rs
@@ -1,7 +1,9 @@
 //! CLINT: *C*ore *L*ocal *Int*errupt
 
-use super::MmioDevice;
-use crate::memmap::MemoryMap;
+use super::{MmioDevice, PTE_FLAGS_FOR_DEVICE};
+use crate::memmap::{
+    GuestPhysicalAddress, HostPhysicalAddress, MemoryMap, page_table::constants::PAGE_SIZE,
+};
 
 use alloc::vec::Vec;
 use fdt::{Fdt, standard_nodes::MemoryRegion};
@@ -32,7 +34,24 @@ impl MmioDevice for Clint {
         self.register_map_regions
             .clone()
             .into_iter()
-            .map(|region| MemoryMap::from(region))
+            .map(|region| {
+                let mut size = region.size.unwrap();
+                let mut virt_start = GuestPhysicalAddress(region.starting_address as usize);
+                let mut phys_start = HostPhysicalAddress(region.starting_address as usize);
+
+                // change memory map region if region size is less than the page size.
+                if phys_start % PAGE_SIZE != 0 && size < PAGE_SIZE {
+                    size = PAGE_SIZE;
+                    virt_start = virt_start - (virt_start % PAGE_SIZE);
+                    phys_start = phys_start - (phys_start % PAGE_SIZE);
+                }
+
+                MemoryMap::new(
+                    virt_start..virt_start + size,
+                    phys_start..phys_start + size,
+                    &PTE_FLAGS_FOR_DEVICE,
+                )
+            })
             .collect()
     }
 }
