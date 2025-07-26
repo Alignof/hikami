6eaeffa070c7d85a7224a1eb9f2eb78af3f94fc7
Alingof <Alignof@outlook.com>
2025-07-26 17:31:46 +0900

    [update] remove an implementation of `VirtIO::try_new` and add `VirtIO::new_with_node`

--- hikami_core/src/device/virtio.rs
+++ hikami_core/src/device/virtio.rs
@@ -5,7 +5,7 @@ use crate::memmap::MemoryMap;
 
 use alloc::vec::Vec;
 use core::slice::Iter;
-use fdt::{Fdt, standard_nodes::MemoryRegion};
+use fdt::{Fdt, node::FdtNode, standard_nodes::MemoryRegion};
 
 /// A virtualization standard for network and disk device drivers.
 /// Since more than one may be found, we will temporarily use the first one.
@@ -46,6 +46,22 @@ pub struct VirtIo {
 }
 
 impl VirtIo {
+    /// Create self with fdt node
+    pub fn new_with_node(root_page_table_addr: HostPhysicalAddress, virtio_node: &FdtNode) -> Self {
+        let register_map_regions: Vec<MemoryRegion> = virtio_node.reg().unwrap().collect();
+
+        Self::create_page_table(
+            root_page_table_addr,
+            &register_map_regions,
+            virtio_node.name,
+        );
+
+        VirtIo {
+            register_map_regions,
+            irq: virtio_node.property("interrupts").unwrap().value[0],
+        }
+    }
+
     /// Return `irq`.
     pub fn irq(&self) -> u8 {
         self.irq
@@ -53,16 +69,12 @@ impl VirtIo {
 }
 
 impl MmioDevice for VirtIo {
-    fn try_new(device_tree: &Fdt, compatibles: &[&str]) -> Option<Self> {
-        let node = device_tree.find_compatible(compatibles)?;
-        let register_map_regions: Vec<MemoryRegion> = node.reg().unwrap().collect();
-
-        let irq = node.property("interrupts").unwrap().value[0];
-
-        Some(VirtIo {
-            register_map_regions,
-            irq,
-        })
+    fn try_new(
+        _root_page_table_addr: HostPhysicalAddress,
+        _device_tree: &Fdt,
+        _compatibles: &[&str],
+    ) -> Option<Self> {
+        unreachable!("use `VirtIo::new_with_node` instead.")
     }
 
     fn memmap(&self) -> Vec<MemoryMap> {
