265e3b83b5450fb7b449af2c53f2820f160cf504
Alignof <Alignof@outlook.com>
2025-09-08 14:45:45 +0900

    [add] add flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      fenix,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
+        };
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "rust environment";
+          nativeBuildInputs = [
+            toolchain
+            pkgs.dtc
+          ];
+        };
+      }
+    );
+}
