381d295fbae6d155ba2cdff8c52a0efd7fe8089b
Alignof <Alignof@outlook.com>
2025-09-18 20:23:02 +0900

    [!][update] update memory layout of the hikami

--- hikami_core/src/lib.rs
+++ hikami_core/src/lib.rs
@@ -100,13 +100,13 @@ impl HypervisorData {
                 G_STAGE_PTE_FLAGS,
             ),
             MemoryMap::new(
-                GuestPhysicalAddress(0x1_1000_0000)..GuestPhysicalAddress(0x4_8000_0000),
-                HostPhysicalAddress(0x1_1000_0000)..HostPhysicalAddress(0x4_8000_0000),
+                GuestPhysicalAddress(0x1_8000_0000)..GuestPhysicalAddress(0x4_8000_0000),
+                HostPhysicalAddress(0x1_8000_0000)..HostPhysicalAddress(0x4_8000_0000),
                 G_STAGE_PTE_FLAGS,
             ),
             MemoryMap::new(
-                GuestPhysicalAddress(0x80_0000_0000)..GuestPhysicalAddress(0x180_0000_0000),
-                HostPhysicalAddress(0x80_0000_0000)..HostPhysicalAddress(0x180_0000_0000),
+                GuestPhysicalAddress(0x80_0000_0000)..GuestPhysicalAddress(0x200_0000_0000),
+                HostPhysicalAddress(0x80_0000_0000)..HostPhysicalAddress(0x200_0000_0000),
                 G_STAGE_PTE_FLAGS,
             ),
         ];
